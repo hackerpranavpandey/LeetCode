b796750b91d1734e3e3a365b48e149394d25982b
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-06-17 08:50:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        int last_index=pow(c,0.5);
+        for(int i=last_index;i>=0;i--){
+            int t_1=pow(i,2);
+            double t_2=pow(c-t_1,0.5);
+            if((t_2-static_cast<int>(t_2))==0)
+                return true;
+        }
+        return false;
+    }
+};
