19164e34bb6b54ffc591c3cf54ab47f661e6a4a2
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-06-17 08:54:42 +0530

    Time: 5 ms (34.50%), Space: 7.1 MB (18.45%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        int last_index=pow(c,0.5); 
+        int last_index=pow(c,0.5); // it is maximum index till where sum of the square is possible
         for(int i=last_index;i>=0;i--){
             int t_1=pow(i,2);
-            double t_2=pow(c-t_1,0.5);
+            double t_2=pow(c-t_1,0.5);          // check if c-i^2 is an integer
             if((t_2-static_cast<int>(t_2))==0)
                 return true;
         }
