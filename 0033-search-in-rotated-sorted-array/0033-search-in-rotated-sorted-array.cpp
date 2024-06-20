a9bc55f542203ab8a016e3c32323abecd37f1951
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-06-20 20:00:39 +0530

    Time: 4 ms (40.49%), Space: 13.4 MB (96.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)
-                return i;
+    int search(vector<int>& arr, int target) {
+        // since it is sorted and search
+        // lets use binary search
+        // then there is a small check that for each arr[mid] check which part is sorted
+        // then accodingly compare the sorted section with section and check if target is there
+        // so there will be two condition that eithere left or right half will be sorted
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            // bas 3 condition lagane hai yaha pe
+            if(arr[mid]==target)
+                return mid;
+            // agar left half sorted hai tab
+            else if(arr[mid]>=arr[low]){
+                // check if target is in left half since here it is sorted
+                if(target>=arr[low] && target<arr[mid]){
+                    high=mid-1;
+                }
+                else
+                    low=mid+1;
+            }
+            // ye case tab hoga jab right half sorted hai
+            else{
+                // check if target is in left half since here it is sorted
+                if(target>arr[mid] && target<=arr[high]){
+                    low=mid+1;
+                }
+                else
+                    high=mid-1;
+            }
         }
         return -1;
     }
