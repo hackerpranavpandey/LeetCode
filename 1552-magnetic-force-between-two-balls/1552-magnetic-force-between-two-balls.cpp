bc96683e1ee427fd4b90649d13099f07037a06a8
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-06-20 08:54:26 +0530

    Time: 123 ms (69.44%), Space: 78.9 MB (9.76%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,9 +1,12 @@
 class Solution {
+    // ye function batega ki kitne ball hai jinke beech ka magnetic force gap hai
 private:
     int number_of_balls(vector<int> position,int gap){
-        int n_p=1;
+        // pehle ball hmesha hoga hi
         int current=position[0];
+        int n_p=1;
         for(int i=1;i<position.size();i++){
+    // agar gap ya jyada magnetic force hai tab current position ko update karo aur n_p ko increase karo
             if(position[i]-current >= gap){
                 n_p+=1;
                 current=position[i];
@@ -14,9 +17,11 @@ private:
 public:
     int maxDistance(vector<int>& position, int m) {
         int n=position.size();
+        // wo minimum gap nukalo jisme m balls rakh sako
+        // binary seach lagate hai 
         sort(position.begin(),position.end());
         int ans=0;
-        int start=0;
+        int start=1; 
         int end=position[n-1]/(m-1);
         while(start<=end){
             int mid=(start+end)/2;
