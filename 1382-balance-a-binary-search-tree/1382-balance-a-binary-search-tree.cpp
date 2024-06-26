20911ea061abfa6c8e5d45667ede68e746650751
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-06-26 08:29:39 +0530

    Time: 103 ms (33.15%), Space: 62.5 MB (60.81%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -20,11 +20,6 @@ public:
         sorted_array.push_back(root->val);
         Inorder(root->right);
     }
-    // now to construct balance BST
-    // just simple one logic use it recursively
-    // break the sorted array into two half and make mid as root
-    // root->left will be again for mid of first half
-    // root right for mid of second half
     TreeNode* constructBST(int start,int end){
         if(start>end)
             return nullptr;
