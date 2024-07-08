64bddaa229e76b58574f3e9eba92dbf4501350f8
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-08 09:21:43 +0530

    Time: 60 ms (5.04%), Space: 7.6 MB (43.98%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> circular_queue;
+        // insert all into vector
+       vector<int> circular_queue;
        for(int i=1;i<=n;i++){
             circular_queue.push_back(i);
         }
         int remove=0;
         int j=0;
+        // continue till n-1 are removes
         while(remove!=(n-1)){
+            // step will tell k numbers covered
             int step=0;
             while(step!=k){
+                // count only if not equals -1
                  if(circular_queue[j]!=-1)
                     step++;
                  if(step<k)
                      j=(j+1)%n;
             }
+            // make it -1 now considering it removed
             circular_queue[j]=-1;
             remove++;
         }
