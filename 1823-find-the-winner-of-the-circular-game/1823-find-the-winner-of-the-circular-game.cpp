c1298d885bfaa2026ce4b955e43f98ac2640803d
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-09 23:23:20 +0530

    Time: 24 ms (22.83%), Space: 25.7 MB (9.27%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -2,34 +2,47 @@ class Solution {
 public:
     int findTheWinner(int n, int k) {
         // insert all into vector
-       vector<int> circular_queue;
-       for(int i=1;i<=n;i++){
-            circular_queue.push_back(i);
+       // vector<int> circular_queue;
+       // for(int i=1;i<=n;i++){
+       //      circular_queue.push_back(i);
+       //  }
+       //  int remove=0;
+       //  int j=0;
+       //  // continue till n-1 are removes
+       //  while(remove!=(n-1)){
+       //      // step will tell k numbers covered
+       //      int step=0;
+       //      while(step!=k){
+       //          // count only if not equals -1
+       //           if(circular_queue[j]!=-1)
+       //              step++;
+       //           if(step<k)
+       //               j=(j+1)%n;
+       //      }
+       //      // make it -1 now considering it removed
+       //      circular_queue[j]=-1;
+       //      remove++;
+       //  }
+       //  int ans=-1;
+       //  for(int i=0;i<n;i++){
+       //      if(circular_queue[i]!=-1){
+       //          ans=circular_queue[i];
+       //          break;
+       //      }
+       //  }
+       //  return ans;
+        // simple simulation using queue
+        queue<int> circular;
+        for(int i=1;i<=n;i++){
+            circular.push(i);
         }
-        int remove=0;
-        int j=0;
-        // continue till n-1 are removes
-        while(remove!=(n-1)){
-            // step will tell k numbers covered
-            int step=0;
-            while(step!=k){
-                // count only if not equals -1
-                 if(circular_queue[j]!=-1)
-                    step++;
-                 if(step<k)
-                     j=(j+1)%n;
+        while(circular.size()>1){
+            for(int i=0;i<k-1;i++){
+                circular.push(circular.front());
+                circular.pop();
             }
-            // make it -1 now considering it removed
-            circular_queue[j]=-1;
-            remove++;
+            circular.pop();
         }
-        int ans=-1;
-        for(int i=0;i<n;i++){
-            if(circular_queue[i]!=-1){
-                ans=circular_queue[i];
-                break;
-            }
-        }
-        return ans;
+        return circular.front();
     }
 };
