1a42d46de5c25ae83150a2b5dfad992c22035e4f
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-11 14:55:38 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (50.36%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<int> paranthesis_index;
+        // stack<int> paranthesis_index;
+        // string ans="";
+        // for(char c:s){
+        //     if(c=='('){
+        //         paranthesis_index.push(ans.size());
+        //     }
+        //     else if(c==')'){
+        //         // get the index of start of '(' of present ')'
+        //         int start=paranthesis_index.top();
+        //         paranthesis_index.pop();
+        //         // reverse
+        //         reverse(ans.begin()+start,ans.end());
+        //     }
+        //     else
+        //         ans+=c;
+        // }
+        // return ans;
+        // above is best way to solve this
+        int n_b=0;  // find number of bracket
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')
+                n_b++;
+        }
+        vector<int> b_i(n_b,0);   // index of all bracket '('
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                b_i[j]=i;
+                j++;
+            }
+            else if(s[i]==')'){
+                j--;
+                reverse(s.begin()+b_i[j]+1,s.begin()+i);  // do not include beacket
+            }
+        }
         string ans="";
         for(char c:s){
-            if(c=='('){
-                paranthesis_index.push(ans.size());
-            }
-            else if(c==')'){
-                // get the index of start of '(' of present ')'
-                int start=paranthesis_index.top();
-                paranthesis_index.pop();
-                // reverse
-                reverse(ans.begin()+start,ans.end());
-            }
-            else
+            // add all char which si not bracket
+            if(c!='(' && c!=')')
                 ans+=c;
         }
         return ans;
