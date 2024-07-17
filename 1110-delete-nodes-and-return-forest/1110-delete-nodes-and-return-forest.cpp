9c08ed0a4ef6da82d9c9f6782f6212fa5b778858
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-17 18:17:08 +0530

    Time: 509 ms (5.00%), Space: 191.5 MB (5.00%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* delete_node(TreeNode* node,unordered_set<int> delete_set,vector<TreeNode*>& ans){
+        if(!node){
+            return NULL;
+        }
+        node->left=delete_node(node->left,delete_set,ans);
+        node->right=delete_node(node->right,delete_set,ans);
+        if(delete_set.find(node->val)!=delete_set.end()){
+            if(node->left!=NULL)
+                ans.push_back(node->left);
+            if(node->right!=NULL)
+                ans.push_back(node->right);
+            delete node;
+            return NULL;
+        }
+        return node;
+    }
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        // so here need to ensure that all child are successfully visited before parents
+        // so lets use postorder traversal technique
+        unordered_set<int> delete_set(to_delete.begin(),to_delete.end());
+        vector<TreeNode*> ans;
+        root=delete_node(root,delete_set,ans);
+        if(root){
+            ans.push_back(root);
+        }
+        return ans;
+    }
+};
