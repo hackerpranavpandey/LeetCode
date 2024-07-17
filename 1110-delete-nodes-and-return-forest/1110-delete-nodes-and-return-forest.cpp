24b7fd6222349f0bc9c3649a0edfeafb228f16a7
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-17 18:18:22 +0530

    Time: 494 ms (5.59%), Space: 191.7 MB (5.00%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -15,8 +15,10 @@ private:
         if(!node){
             return NULL;
         }
+        // postorder traversal
         node->left=delete_node(node->left,delete_set,ans);
         node->right=delete_node(node->right,delete_set,ans);
+        // agar node ko delete karna hai tab uske left aur right ko and=s me push karke NULL return karo
         if(delete_set.find(node->val)!=delete_set.end()){
             if(node->left!=NULL)
                 ans.push_back(node->left);
@@ -25,6 +27,7 @@ private:
             delete node;
             return NULL;
         }
+        // nhi to node return karo
         return node;
     }
 public:
@@ -34,6 +37,7 @@ public:
         unordered_set<int> delete_set(to_delete.begin(),to_delete.end());
         vector<TreeNode*> ans;
         root=delete_node(root,delete_set,ans);
+        // case jab root ko delete nhi karna hai
         if(root){
             ans.push_back(root);
         }
