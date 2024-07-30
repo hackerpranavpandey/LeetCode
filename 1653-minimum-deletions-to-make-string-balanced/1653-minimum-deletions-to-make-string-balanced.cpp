1f5dc0a5b39453ada90e6f8f948e665211de6158
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-30 10:14:25 +0530

    Time: 106 ms (37.00%), Space: 24.7 MB (51.04%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -5,6 +5,7 @@ public:
         int n_deletion=0;
         // pehle sare a phir sare b chahiye
         for(char c:s){
+            // agar b aa gaya tab uske baad a nhi aana chahiye dono me se ek ko delete karna hoga
             if(!st.empty() && st.top()=='b' && c=='a'){  // agar stack ke top pe a hai aur stack khali nhi hai aur abhi b hai tab delete karo top ko
                 st.pop();
                 n_deletion++;
