cc719269840e5fd65978157d427a1e38b9e3edb8
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-07-30 20:24:19 +0530

    Time: 3 ms (58.25%), Space: 9.1 MB (12.44%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long ans=0;
+        int i=0,n=s.size();
+        bool isNegative=false; // it will tell if the number is negative or not
+        // ignore leading whitespace
+        while(i<n && s[i]==' '){
+            i++;
+        }
+        // check if it is negative
+        if(i<n && s[i]=='-'){
+            i++;
+            isNegative=true;
+        }
+        // case for positive
+        else if(s[i]=='+'){
+            i++;
+        }
+        // now just convert to int and keep on multiplying with 10 and update
+        while(i<n && isdigit(s[i])){
+            int num=s[i]-'0';
+            ans*=10;
+            ans+=num;
+            if(ans>INT_MAX){
+                return isNegative?INT_MIN:INT_MAX;
+            }
+            i++;
+        }
+        if(isNegative==true) // agar negative hai tab
+            ans*=-1;
+        if(ans<INT_MIN)
+            return INT_MIN;
+        else if(ans>INT_MAX)
+            return INT_MAX;
+        else
+            return static_cast<int>(ans);
+    }
+};
