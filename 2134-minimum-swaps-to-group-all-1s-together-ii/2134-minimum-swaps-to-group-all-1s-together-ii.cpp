b46eee1594e1c8d9a15d7c8e1f252be0f6769130
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-02 11:06:09 +0530

    Time: 59 ms (91.16%), Space: 86.6 MB (43.42%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        // n=below code is logically correct but exceeds time limit
+        // since we know the size of subarray will be same as number of ones
+        // so for wach such subarray compute number of swap required
+        // it will be equal to number of zerosfor in that subarray
+        // below code is logically correct but exceeds time limit
         // int n=nums.size();
         // int num_one=0;
         // for(int i=0;i<n;i++){
@@ -26,9 +29,11 @@ public:
         //     i+=1;
         // }
         // return ans;
+        // now to avoid time complexity issue
+        // use vector to store num of zeros till that index including that index
         int n=nums.size();
         int num_one=0;
-        vector<int> num_zeros(n,0);
+        vector<int> num_zeros(n,0); // store num of zeros
         int num_zero=0;
         for(int i=0;i<n;i++){
             if(nums[i]==0)
@@ -37,14 +42,16 @@ public:
                 num_one++;
             num_zeros[i]=num_zero;
         }
+        // no swap needed
         if(num_one==n || num_one==0)
             return 0;
-        int ans=num_zeros[num_one-1];
+        int ans=num_zeros[num_one-1]; // first subarray
         for(int i=1;i<n;i++){
             int end_pos=i+num_one-1;
             int swap=0;
             if(end_pos<n)
                 swap=num_zeros[end_pos]-num_zeros[i-1];
+            // since it is circular need to start till inex n-1
             else{
                 end_pos-=n;
                 swap=(num_zeros[n-1]-num_zeros[i-1])+num_zeros[end_pos];
