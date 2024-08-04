f1a2903b1c51f167cae34d02957819b9a9236905
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-04 10:27:34 +0530

    Time: 93 ms (48.76%), Space: 27.3 MB (19.10%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -3,11 +3,12 @@ public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         vector<int> arr;
         // for any index find all subarray possible till that index
-        // store the sum in arr
         // then sort arr and return sum of values from index left-1 to right-1
         for(int i=0;i<n;i++){
             int sum=nums[i];
             arr.push_back(nums[i]);
+            // for eachi from i-1 to 0 
+            // insert all possible subarray sum
             for(int j=i-1;j>=0;j--){
                 sum+=nums[j];
                 arr.push_back(sum);
