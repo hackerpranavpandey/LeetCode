cde16507a2714ea1517970d587dfe6d463abd951
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-08 12:24:46 +0530

    Time: 12 ms (40.04%), Space: 17 MB (17.90%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -19,5 +19,24 @@ public:
                     matrix[i][j]=0;
             }
         }
+        // above algorithm is fine but lets do it in constant space
+        // for that we will use first element of each row and each column as flag to determine
+        //  for(int i=0;i<row;i++){
+        //     for(int j=0;j<col;j++){
+        //         if(matrix[i][j]==0){
+        //             matrix[i][0]=0;
+        //             if(j!=0)
+        //                 matrix[0][j]=0;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<row;i++){
+        //     for(int j=0;j<col;j++){
+        //         if(matrix[i][j]!=0){
+        //             if(matrix[i][0]==0 || matrix[0][j]==0)
+        //                 matrix[i][j]=0;
+        //         }
+        //     }
+        // }
     }
 };
