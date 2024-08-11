99768245e4b082866093e47309f666dbdaa07c12
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-11 19:15:39 +0530

    Time: 162 ms (14.37%), Space: 51.3 MB (15.80%) - LeetHub

--- 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
@@ -1,5 +1,7 @@
 class Solution {
 private:
+    // helper function the return number of island in a grid
+    // using dfs
     int num_island(vector<vector<int>> grid){
         int n=grid.size();
         int m=grid[0].size();
@@ -27,28 +29,29 @@ private:
     }
 public:
     int minDays(vector<vector<int>>& grid) {
+        // first check numberof island present
+        // if it is not equal to 1 then no need
+        // now analyse the problem it will take at most 2 days to make any grid disconnected
+        // now for each grid[i][j]==1 make it 0
+        // then check number of island now if not equal to 1
+        // so in such condition we need just one day
+        // for all rest conditions need 2 days
         int number_of_islands=num_island(grid);
         if(number_of_islands!=1)
             return 0;
-        vector<int> day_req;
+        int ans=2;
         int row=grid.size();
         int col=grid[0].size();
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1){
                     grid[i][j]=0;
-                    if(num_island(grid)==1)
-                        day_req.push_back(2);
-                    else
-                        day_req.push_back(1);
+                    if(num_island(grid)!=1)
+                        ans=1;
                     grid[i][j]=1;
                 }
             }
         }
-        int min_day=2;
-        for(auto& it:day_req){
-            min_day=min(min_day,it);
-        }
-        return min_day;
+        return ans;
     }
 };
