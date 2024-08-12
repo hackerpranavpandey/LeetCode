f2b3f1c0222d0d01d4b648a65357cb2b88b121f4
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-13 02:18:59 +0530

    Time: 6 ms (83.51%), Space: 11.2 MB (70.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ans=0;
-        int i=0;
+        // int ans=0;
+        // int i=0;
+        // int n=s.size();
+        // while(i<n){
+        //     unordered_set<char> element;
+        //     int start=i;
+        //     while(start<n){
+        //         if(element.find(s[start])==element.end()){
+        //             element.insert(s[start]);
+        //             start++;
+        //         }
+        //         else
+        //             break;
+        //     }
+        //     ans=max(ans,start-i);
+        //     i++;
+        // }
+        // return ans;
+        vector<int> mpp(256,-1);
+        int left=0,right=0;
         int n=s.size();
-        while(i<n){
-            unordered_set<char> element;
-            int start=i;
-            while(start<n){
-                if(element.find(s[start])==element.end()){
-                    element.insert(s[start]);
-                    start++;
-                }
-                else
-                    break;
-            }
-            ans=max(ans,start-i);
-            i++;
+        int ans=0;
+        while(right<n){
+            if(mpp[s[right]]!=-1)
+                left=max(mpp[s[right]]+1,left);
+            mpp[s[right]]=right;
+            ans=max(ans,right-left+1);
+            right++;
         }
         return ans;
     }
