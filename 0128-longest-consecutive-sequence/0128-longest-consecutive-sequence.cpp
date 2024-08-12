08a68162196ddc8e0b35f3b66971806a2d533de9
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-13 01:08:05 +0530

    Time: 113 ms (68.11%), Space: 74.7 MB (27.23%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,26 +2,44 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         // idea using sorting
-        if(nums.size()==0)
-            return 0;
-        sort(nums.begin(),nums.end());
-        int start=nums[0];
-        int ans=1;
-        int cnt=1;
-        int i=1;
-        while(i<nums.size()){
-            if(nums[i]==(start+1)){
-                 cnt++;
-                 start=nums[i];
-            }
-            else if((nums[i]-1)>start){
-                ans=max(ans,cnt);
-                start=nums[i];
-                cnt=1;
-            }
-            i++;
+        // if(nums.size()==0)
+        //     return 0;
+        // sort(nums.begin(),nums.end());
+        // int start=nums[0];
+        // int ans=1;
+        // int cnt=1;
+        // int i=1;
+        // while(i<nums.size()){
+        //     if(nums[i]==(start+1)){
+        //          cnt++;
+        //          start=nums[i];
+        //     }
+        //     else if((nums[i]-1)>start){
+        //         ans=max(ans,cnt);
+        //         start=nums[i];
+        //         cnt=1;
+        //     }
+        //     i++;
+        // }
+        // ans=max(ans,cnt);
+        // return ans;
+        // if value less than this is not present then only start searching for consecutive next number
+        unordered_set<int> sequence;
+        for(int num:nums){
+            sequence.insert(num);
         }
-        ans=max(ans,cnt);
+        int ans=0;
+        for(auto it:sequence){
+            if(sequence.find(it-1)==sequence.end()){
+                int cnt=1;
+                int value=it+1;
+                while(sequence.find(value)!=sequence.end()){
+                    value++;
+                    cnt++;
+                }
+                ans=max(ans,cnt);
+            }
+        }
         return ans;
     }
 };
