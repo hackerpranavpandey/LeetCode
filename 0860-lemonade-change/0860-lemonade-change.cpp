c9297d33676049cb42f60c29d2e346982a5ec1f7
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-15 10:17:18 +0530

    Time: 72 ms (57.53%), Space: 86.2 MB (9.36%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int coin_5=0,coin_10=0,coin_20=0;
+        for(auto bill:bills){
+            if(bill==5)
+                coin_5+=1;
+            else if(bill==10){
+                if(coin_5==0)
+                    return false;
+                coin_10+=1,coin_5--;
+            }
+            else{
+                if(coin_5==0 || (coin_5*5+coin_10*10)<15)
+                    return false;
+                coin_20++;
+                if(coin_10!=0){
+                    coin_10--,coin_5--;
+                }
+                else
+                    coin_5-=3;
+            }
+        }
+        return true;
+    }
+};
