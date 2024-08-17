108811a8474234106a9c1f672dc8e0a41d2b96aa
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-17 17:16:20 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (42.38%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,16 +11,46 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& ans){
-        if(root){
-            inorder(root->left,ans);
-            ans.push_back(root->val);
-            inorder(root->right,ans);
-        }
-    }
+    // void inorder(TreeNode* root,vector<int>& ans){
+    //     if(root){
+    //         inorder(root->left,ans);
+    //         ans.push_back(root->val);
+    //         inorder(root->right,ans);
+    //     }
+    // }
     vector<int> inorderTraversal(TreeNode* root){
+        // vector<int> ans;
+        // inorder(root,ans);
+        // return ans;
+        // above is recursive way to do this
+        // now lets do intutively using morris traversal
+        TreeNode* curr=root;
         vector<int> ans;
-        inorder(root,ans);
+        while(curr){
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* temp=curr->left;
+                // point left tree right most node to each root node
+                // but specia cheking condition whether to move right or left from current node
+                while(temp->right!=NULL && temp->right!=curr){
+                    temp=temp->right;
+                }
+                // threading concept to be used here
+                // when righmost node of left subtree points to null then move to leftmost tree
+                if(temp->right!=curr){
+                    temp->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    ans.push_back(curr->val);
+                    temp->right=NULL;
+                    curr=curr->right;
+                }
+            }
+        }
         return ans;
     }
 };
