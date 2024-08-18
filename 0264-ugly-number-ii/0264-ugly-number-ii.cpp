2435a56fad8f752e7de07ee15929538d4dbf8fbb
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-18 18:46:38 +0530

    Time: 76 ms (28.13%), Space: 33.9 MB (14.22%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         // so start from 1 now whatever ugly number comes has to be miltiple of 2 3 or 5
+        // so using the conventional approach it exceeds the time limit
+        // now what we can do is first take 1 then keep on multiplying it with 2,3 and 5
+        // now need to do this for all generated ugly numbers but the problem comes with duplicates
+        // and also if some bigger comes before smaller
+        // so use priority queue as min heap then do this
+        // for issue of duplicated push the value after multiplying with 2,3 and 5
+        // then insert themm in set
+        // after that for next value push only if it is not present in the set
         priority_queue<long,vector<long>,greater<long>> ugly_numbers;
         ugly_numbers.emplace(1);
         unordered_set<long long> ugly_seen;
