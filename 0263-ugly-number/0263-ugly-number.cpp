a94470d4e53f33bf3eab9d5e1a1c29b5e0814ea8
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-18 12:17:34 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (41.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -4,17 +4,28 @@ public:
         // base case
         if(n==2 || n==3 || n==5)
             return true;
+        // it should be postive not even equal to 0
         else if(n<=0)
             return false;
-        int start=2;
-        while(start<=5 && n>1){
-            if(n%start==0){
-                n/=start;
-            }
-            else
-                start+=1;
-        }
-        if(n>1)
+        // int start=2;
+        // while(start<=5 && n>1){
+        //     if(n%start==0){
+        //         n/=start;
+        //     }
+        //     else
+        //         start+=1;
+        // }
+        // if(n>1)
+        //     return false;
+        // return true;
+        // above is correct but not a good way to write code
+        while(n%2==0)
+            n=n/2;
+        while(n%3==0)
+            n=n/3;
+        while(n%5==0)
+            n=n/5;
+        if(n!=1)
             return false;
         return true;
     }
