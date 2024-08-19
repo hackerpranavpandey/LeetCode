59124eea30981c80dda7b747b7da258c3c394c23
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-19 19:13:33 +0530

    Time: 2 ms (43.83%), Space: 12.2 MB (22.58%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        // base case
         if(p==NULL && q==NULL)
             return true;
         else if(p==NULL && q!=NULL)
@@ -19,6 +20,7 @@ public:
         else if(p!=NULL && q==NULL)
             return false;
         else{
+            // if both node value is same check left and right tree
             if(p->val==q->val)
                 return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
             else
