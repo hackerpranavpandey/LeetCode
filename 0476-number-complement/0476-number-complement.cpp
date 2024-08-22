7a5448d3f4e46ce6dd60a37fe3f8a6b922c5c9a1
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-22 11:32:32 +0530

    Time: 3 ms (41.07%), Space: 7.6 MB (8.34%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int ans=0;
-        int i=0;
+        int ans=0,i=0;
         while(num>0){
             int bit=num&1;
             if(bit==0) ans+=pow(2,i);
-            num=num>>1;
-            i++;
+            num=num>>1,i++;
         }
         return ans;
     }
