d8dcb6729b127e4ad01ad71895cf38abdf493e2d
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-22 20:40:42 +0530

    Time: 16 ms (67.41%), Space: 18 MB (15.14%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,14 +2,22 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
+        // assuing two different set to store node 
+        // queue ds 
+        // vis set to avoid revisiting
+        // it may not be fully connected so traverse for each node
         unordered_set<int> a,b;
         queue<int> t;
         unordered_set<int> vis;
         for(int i=0;i<n;i++){
+            // if already visited move to next node
             if(vis.find(i)!=vis.end())
                 continue;
+            // push in set a
             a.emplace(i);
             t.push(i);
+            // try bfs with condition that if current node is in a then adjacent should be in b
+            // and vice versa also
             while(!t.empty()){
                 auto it=t.front();
                 t.pop();
