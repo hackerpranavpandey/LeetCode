5225c8cb3183f6d55d02af95111421febd6a4b01
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-23 16:20:49 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (17.44%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -1,80 +1,61 @@
 class Solution {
 private:
-    int find_lcm(int a, int b) {
-        if (a % b == 0)
+    // computing the lcm of to numbers
+    int find_lcm(int a,int b){
+        if(a%b==0)
             return a;
-        else if (b % a == 0)
+        else if(b%a==0)
             return b;
-        int lcm = a * b, min_num = min(a, b), max_div = 1;
-        for (int i = 2; i <= min_num; i++) {
-            if (a % i == 0 && b % i == 0)
-                max_div = i;
+        int lcm=a*b,min_num=min(a,b),max_div=1;
+        for(int i=2;i<=min_num;i++){
+            if(a%i==0 && b%i==0)
+                max_div=i;
         }
-        return lcm / max_div;
+        return lcm/max_div;
     }
-
-    int find_hcf(int a, int b) {
-        int hcf = 1;
-        for (int i = 2; i <= min(a, b); i++) {
-            if (a % i == 0 && b % i == 0)
-                hcf = i;
+    // then computing the hcf 
+     int find_hcf(int a,int b){
+        int hcf=1;
+        for(int i=2;i<=min(a,b);i++){
+            if(a%i==0 && b%i==0)
+                hcf=i;
         }
         return hcf;
     }
-
 public:
     string fractionAddition(string expression) {
-        vector<int> den;
-        vector<int> num;
-        int i = 0, n = expression.size();
-
-        while (i < n) {
-            int sign = 1;
-            if (expression[i] == '+' || expression[i] == '-') {
-                if (expression[i] == '-')
-                    sign = -1;
+        vector<int> num,den;
+        int i=0,n=expression.size();
+        while(i<n){
+            string numerator="",denominator="";
+            // extract numerator
+            while(expression[i]!='/'){
+                numerator+=expression[i],i++;
+            }
+            num.push_back(stoi(numerator));
+            i++;
+            // then denominator
+            while((expression[i]!='+' && expression[i]!='-') && i<n){
+                denominator+=expression[i];
                 i++;
             }
-
-            int numerator = 0;
-            while (i < n && isdigit(expression[i])) {
-                numerator = numerator * 10 + (expression[i] - '0');
-                i++;
-            }
-            numerator *= sign;
-
-            if (i < n && expression[i] == '/') {
-                i++;
-                int denominator = 0;
-                while (i < n && isdigit(expression[i])) {
-                    denominator = denominator * 10 + (expression[i] - '0');
-                    i++;
-                }
-                num.push_back(numerator);
-                den.push_back(denominator);
-            }
+            den.push_back(stoi(denominator));
         }
-
-        // Calculate the LCM of all denominators
-        int lcm = den[0];
-        for (int i = 1; i < den.size(); i++) {
-            lcm = find_lcm(lcm, den[i]);
+        int lcm=den[0];
+        for(int i=1;i<den.size();i++){
+            lcm=find_lcm(lcm,den[i]);
         }
-
-        // Adjust numerators to the common denominator and calculate the sum
-        int sum = 0;
-        for (int i = 0; i < num.size(); i++) {
-            num[i] = num[i] * (lcm / den[i]);
-            sum += num[i];
+        // now sum up all the numerators
+        int sum=0;
+        for(int i=0;i<num.size();i++){
+            num[i]=(num[i]*(lcm/den[i]));
+            sum+=num[i];
         }
-
-        if (sum == 0)
+        // in case sum comes out to be 0
+        if(sum==0)
             return "0/1";
-
-        int hcf = find_hcf(abs(sum), lcm);
-        sum /= hcf;
-        lcm /= hcf;
-
-        return to_string(sum) + "/" + to_string(lcm);
+        int hcf=find_hcf(abs(sum),lcm);
+        sum/=hcf,lcm/=hcf;
+        return to_string(sum)+"/"+to_string(lcm);
     }
 };
