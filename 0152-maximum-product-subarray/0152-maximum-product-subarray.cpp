596ed999bf4e634edb61ee986c8d3ac838847305
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-27 14:55:39 +0530

    Time: 3 ms (84.64%), Space: 16.6 MB (40.47%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         // below algorithm is fine but it takes O(n^2) time
-        int ans=INT_MIN,n=nums.size();
-        for(int i=0;i<n;i++){
-            int product=nums[i],t;
-            t=product;
-            for(int j=i+1;j<n;j++){
-                product*=nums[j];
-                t=max(t,product);
-            }
-            ans=max(ans,t);
+        // int ans=INT_MIN,n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     int product=nums[i],t;
+        //     t=product;
+        //     for(int j=i+1;j<n;j++){
+        //         product*=nums[j];
+        //         t=max(t,product);
+        //     }
+        //     ans=max(ans,t);
+        // }
+        // return ans;
+        // lets use the concept of kadane algorithm
+        // but twice once from forwqrd another from backward
+        int max_value=INT_MIN;
+        int prod=1;
+        for(int num:nums){
+            prod*=num;
+            max_value=max(max_value,prod);
+            if(!prod)
+                prod=1;
         }
-        return ans;
-        // lets try to apply kandanes algorithms twice
+        prod=1;
+        for(int j=nums.size()-1;j>=0;j--){
+            prod*=nums[j];
+            max_value=max(max_value,prod);
+            if(!nums[j])
+                prod=1;
+        }
+        return max_value;
     }
 };
