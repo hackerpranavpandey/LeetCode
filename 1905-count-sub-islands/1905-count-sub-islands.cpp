07260210c10c125f07beaaaf0f69de2580bae494
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-28 13:26:00 +0530

    Time: 348 ms (26.73%), Space: 246.1 MB (6.57%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    // the task of this function is to count 
+    bool part_of_island(vector<vector<int>>& grid1,vector<vector<int>>& grid2,int i ,int j){
+        int m=grid1.size(),n=grid1[0].size();
+        queue<pair<int,int>> t;
+        vector<pair<int,int>> index;
+        t.push({i,j});
+        while(!t.empty()){
+            auto it=t.front();
+            t.pop();
+            int r=it.first,c=it.second;
+            index.push_back(it);
+            if(r+1<m && grid2[r+1][c]==1){
+                grid2[r+1][c]=0;
+                t.push({r+1,c});
+            }
+            if(c+1<n && grid2[r][c+1]==1){
+                grid2[r][c+1]=0;
+                t.push({r,c+1});
+            }
+            if(r-1>=0 && grid2[r-1][c]==1){
+                grid2[r-1][c]=0;
+                t.push({r-1,c});
+            }
+            if(c-1>=0 && grid2[r][c-1]==1){
+                grid2[r][c-1]=0;
+                t.push({r,c-1});
+            }
+        }
+        for(auto it:index){
+            if(grid1[it.first][it.second]!=1)
+                return false;
+        }
+        return true;
+    }
+public:
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        // so the idea is same as from the number of island problem 
+        // just one difference here is that
+        // here if the island is there in grid2 then all the corresponding cells in grid1 
+        // should also be 0
+        int ans=0;
+        int m=grid1.size(),n=grid1[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid2[i][j]==1){
+                    if(part_of_island(grid1,grid2,i,j)==true)
+                        ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
