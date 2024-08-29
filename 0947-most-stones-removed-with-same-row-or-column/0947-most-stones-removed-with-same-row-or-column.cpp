bf13ecaf728abef4c84ab3674067247a1cb434ec
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-29 23:21:20 +0530

    Time: 96 ms (21.97%), Space: 20.5 MB (68.45%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,48 +1,33 @@
 class Solution {
+private:
+    void dfs(vector<vector<int>>& adj,vector<bool>& vis,int stone){
+        vis[stone]=true;
+        for(int neighbour:adj[stone]){
+            if(vis[neighbour]==false)
+                dfs(adj,vis,neighbour);
+        }
+    }
 public:
     int removeStones(vector<vector<int>>& stones) {
-        int n = stones.size();
-
-        // Adjacency list to store graph connections
-        vector<vector<int>> adjacencyList(n);
-
-        // Build the graph: Connect stones that share the same row or column
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (stones[i][0] == stones[j][0] ||
-                    stones[i][1] == stones[j][1]) {
-                    adjacencyList[i].push_back(j);
-                    adjacencyList[j].push_back(i);
-                }
-            }
-        }
-
-        int numOfConnectedComponents = 0;
-        vector<bool> visited(n, false);
-
-        // Traverse all stones using DFS to count connected components
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                depthFirstSearch(adjacencyList, visited, i);
-                numOfConnectedComponents++;
-            }
-        }
-
-        // Maximum stones that can be removed is total stones minus number of
-        // connected components
-        return n - numOfConnectedComponents;
-    }
-
-private:
-    // DFS to visit all stones in a connected component
-    void depthFirstSearch(vector<vector<int>>& adjacencyList,
-                          vector<bool>& visited, int stone) {
-        visited[stone] = true;
-
-        for (int neighbor : adjacencyList[stone]) {
-            if (!visited[neighbor]) {
-                depthFirstSearch(adjacencyList, visited, neighbor);
-            }
-        }
+      int n=stones.size();
+      // adj list for connected components
+      vector<vector<int>> adj(n);
+      for(int i=0;i<n;i++){
+          for(int j=i+1;j<n;j++){
+              // either row or column matches
+              if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1])
+                  adj[i].push_back(j),adj[j].push_back(i);
+          }
+      }
+      int num=0;
+      vector<bool> vis(n,false);
+      for(int i=0;i<n;i++){
+          // if 1 is fint hen mark visited to all connected components
+          if(!vis[i]){
+              dfs(adj,vis,i);
+              num++;
+          }
+      }
+      return n-num;
     }
 };
