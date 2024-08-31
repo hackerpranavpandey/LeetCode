0293de81d577f83f382aa6418c088a515c3d1fc8
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-31 10:36:03 +0530

    Time: 135 ms (31.19%), Space: 73.3 MB (20.86%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        // so it is simple pplication of dijkstras algorithm
-        // instead of weight use probability and max heap instead of min heap
-        // lets make adjacent list at first
-        vector<pair<int,double>> adj[n];  // each adj[i] will have asjacent nodes and probability
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0],v=edges[i][1];
+        vector<pair<int,double>> adj[n];
+        int i=0;
+        for(vector<int> edge:edges){
+            int u=edge[0],v=edge[1];
             double prob=succProb[i];
-            adj[u].push_back({v,prob}),adj[v].push_back({u,prob});
+            adj[u].push_back({v,prob}),adj[v].push_back({u,prob}),i++;
         }
-        // no instead of weights in case of dijkstra use probability for traversal here
-        priority_queue<pair<double,int>> p_q;
-        p_q.push({1,start_node});  // probability for reaching start_node form start_node is 1
         vector<double> dist(n,0);
+        priority_queue<pair<double,int>> p_q;  // prob,node
+        p_q.push({1,start_node});
         dist[start_node]=1.0;
         while(!p_q.empty()){
             auto it=p_q.top();
             p_q.pop();
-            // now visit each adjacent node
             for(auto t:adj[it.second]){
-                double prob=t.second;
-                int node=t.first;
-                // agar it.second se node reach karne ka probability jyada ha tab
-                // ke case me usko update karke priority queue me push karna hoga
-                if((prob*it.first) > dist[node]){
-                    dist[node]=prob*it.first;
-                    p_q.push({prob*it.first,node});
+                if(dist[t.first]<(it.first*t.second)){
+                    dist[t.first]=it.first*t.second;
+                    p_q.push({it.first*t.second,t.first});
                 }
             }
         }
         return dist[end_node];
-        }
+    }
 };
