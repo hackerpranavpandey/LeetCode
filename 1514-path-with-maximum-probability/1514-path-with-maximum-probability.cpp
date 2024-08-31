c32dabe9fcf8b1866d3cec51dabbd1c311ec05de
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-08-31 10:37:48 +0530

    Time: 149 ms (16.82%), Space: 73.4 MB (20.86%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        // simple concepts of dijkstra algorithm
+        // just replace dtst e=with probabiliy , use max heap and compare condition
         vector<pair<int,double>> adj[n];
         int i=0;
         for(vector<int> edge:edges){
@@ -8,7 +10,7 @@ public:
             double prob=succProb[i];
             adj[u].push_back({v,prob}),adj[v].push_back({u,prob}),i++;
         }
-        vector<double> dist(n,0);
+        vector<double> dist(n,0);   // change from dijkstra
         priority_queue<pair<double,int>> p_q;  // prob,node
         p_q.push({1,start_node});
         dist[start_node]=1.0;
@@ -16,6 +18,7 @@ public:
             auto it=p_q.top();
             p_q.pop();
             for(auto t:adj[it.second]){
+                // change in the compare condition here
                 if(dist[t.first]<(it.first*t.second)){
                     dist[t.first]=it.first*t.second;
                     p_q.push({it.first*t.second,t.first});
