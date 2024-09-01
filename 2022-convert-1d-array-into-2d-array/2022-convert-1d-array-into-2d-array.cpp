9a83598c17c20d2cd8c25aad86c0d617a7ac3d4e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-09-01 11:19:51 +0530

    Time: 101 ms (5.58%), Space: 92.6 MB (41.62%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -3,13 +3,9 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         if(m*n!=original.size())
             return {};
-        vector<vector<int>> ans;
-        for(int i=0;i<m;i++){
-            vector<int> row;
-            for(int j=i*n;j<(i+1)*n;j++){
-                row.push_back(original[j]);
-            }
-            ans.push_back(row);
+        vector<vector<int>> ans(m,vector<int>());
+        for(int i=0;i<original.size();i++){
+            ans[i/n].push_back(original[i]);
         }
         return ans;
     }
