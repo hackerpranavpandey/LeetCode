fb15e4a204e80ee7613b40689b7bad7b81b42318
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-09-09 11:15:55 +0530

    Time: 166 ms (53.94%), Space: 130.5 MB (65.45%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> ans(m,vector<int>(n,-1));
+        int top=0,bottom=m-1,left=0,right=n-1;
+        while(head){
+            for(int i=left;i<=right && head;i++){
+                ans[top][i]=head->val;
+                head=head->next;
+            }
+            for(int i=top+1;i<=bottom && head;i++){
+                ans[i][right]=head->val;
+                head=head->next;
+            }
+            for(int i=right-1;i>=left && head;i--){
+                ans[bottom][i]=head->val;
+                head=head->next;
+            }
+            for(int i=bottom-1;i>=top+1 && head;i--){
+                ans[i][left]=head->val;
+                head=head->next;
+            }
+            top++,bottom--,left++,right--;
+        }
+        return ans;
+    }
+};
