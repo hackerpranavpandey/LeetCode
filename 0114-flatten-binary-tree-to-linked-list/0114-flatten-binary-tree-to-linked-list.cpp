c7c9b86ae06ecd4495885541301e8321f9205234
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-09-16 23:24:30 +0530

    Time: 10 ms (13.66%), Space: 16.9 MB (46.68%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -10,35 +10,44 @@
  * };
  */
 class Solution {
-void preorder(TreeNode* root,vector<int>& node_values){
-    if(!root)
-        return;
-    node_values.push_back(root->val);
-    preorder(root->left,node_values);
-    preorder(root->right,node_values);
-}
+// void preorder(TreeNode* root,vector<int>& node_values){
+//     if(!root)
+//         return;
+//     node_values.push_back(root->val);
+//     preorder(root->left,node_values);
+//     preorder(root->right,node_values);
+// }
 public:
+    TreeNode* prev=nullptr;
     void flatten(TreeNode* root) {
-        // using extra space by storing the node value of preorder in a vector
-        if(!root)
+        // using extra space by storing node in a vector
+        // if(!root)
+        //     return;
+        // vector<int> node_values;
+        // preorder(root,node_values);
+        // int n=node_values.size(),i=0;
+        // TreeNode* temp=root;
+        // while(i<n && temp->right!=NULL){
+        //     temp->val=node_values[i];
+        //     temp->left=NULL;
+        //     temp=temp->right;
+        //     i++;
+        // }
+        // temp->val=node_values[i],temp->left=nullptr;
+        // i++;
+        // while(i<n){
+        //     TreeNode* newNode=new TreeNode(node_values[i]);
+        //     temp->right=newNode;
+        //     temp=temp->right;
+        //     i++;
+        // }
+        // above code is correct but no need to create new node this can be done recursively
+        if(root==nullptr)
             return;
-        vector<int> node_values;
-        preorder(root,node_values);
-        int n=node_values.size(),i=0;
-        TreeNode* temp=root;
-        while(i<n && temp->right!=NULL){
-            temp->val=node_values[i];
-            temp->left=NULL;
-            temp=temp->right;
-            i++;
-        }
-        temp->val=node_values[i],temp->left=nullptr;
-        i++;
-        while(i<n){
-            TreeNode* newNode=new TreeNode(node_values[i]);
-            temp->right=newNode;
-            temp=temp->right;
-            i++;
-        }
+        flatten(root->right);
+        flatten(root->left);
+        root->left=NULL;
+        root->right=prev;
+        prev=root;
     }
 };
