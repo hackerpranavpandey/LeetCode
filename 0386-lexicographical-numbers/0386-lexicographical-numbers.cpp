ba47004f74b84edd8a03bf2bdc1c2397b5c68099
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-09-22 00:57:59 +0530

    Time: 16 ms (55.05%), Space: 13.7 MB (69.49%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    void solve(int t,int n,vector<int>& ans){
+        if(t>n)
+            return;
+        ans.push_back(t);
+        solve(t*10,n,ans);
+        for(int i=1;i<=9;i++){
+            solve(t*10+i,n,ans);   
+        }
+    }
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for(int i=1;i<=9;i++){
+            solve(i,n,ans);   
+        }
+        return ans;
+    }
+};
