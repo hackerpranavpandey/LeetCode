4bf4c425c1c85014b7a90cd0b00db5512b7c1450
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-09-29 16:35:19 +0530

    Time: 99 ms (50.29%), Space: 63.8 MB (12.83%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,56 @@
+class AllOne {
+public:
+    // simple it is easy to do in O(n) for get min or max or O(logn) but not O(1)
+    // so use two map now
+    unordered_map<string,int> count;
+    map<int,unordered_set<string>> freq;
+    // one for the freq count and another the corresponding string
+    // int min_count=INT_MAX,max_count=INT_MIN;
+    // string min_freq="",max_freq="";
+    AllOne() {
+        
+    }
+    
+    void inc(string key) {
+        int t=count[key];
+        count[key]++;
+        if(t>0)
+            freq[t].erase(key);
+        freq[t+1].insert(key);
+        if(freq[t].empty())
+            freq.erase(t);
+    }
+    
+    void dec(string key) {
+        int t=count[key];
+        count[key]--;
+        freq[t].erase(key);
+        if(count[key]==0)
+            count.erase(key);
+        if(t>1)
+            freq[t-1].insert(key);
+        if(freq[t].empty())
+            freq.erase(t);
+    }
+    
+    string getMaxKey() {
+        if(count.empty())
+            return "";
+        return *(freq.rbegin()->second.begin());
+    }
+    
+    string getMinKey() {
+        if(count.empty())
+            return "";
+        return *(freq.begin()->second.begin());
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
