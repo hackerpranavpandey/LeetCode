edd5e786b77649405f67364b47432430ade875a1
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-04 19:27:16 +0530

    Time: 124 ms (26.20%), Space: 71.4 MB (29.51%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,18 +3,34 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         // it is same as number of pairs with sum equals k
         int n=nums.size();
-        int start=0,end=n-1,ans=0;
-        sort(nums.begin(),nums.end());
-        while(start<end){
-            if(nums[start]+nums[end]==k){
-                ans++;
-                start++,end--;
+        // int start=0,end=n-1,ans=0;
+        // sort(nums.begin(),nums.end());
+        // while(start<end){
+        //     if(nums[start]+nums[end]==k){
+        //         ans++;
+        //         start++,end--;
+        //     }
+        //     else if(nums[start]+nums[end]>k){
+        //         end--;
+        //     }
+        //     else{
+        //         start++;
+        //     }
+        // }
+        // return ans;
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int rem=k-nums[i];
+            if(nums[i]==rem){
+                if(mpp[rem]>=2)
+                    mpp[rem]-=2,ans++;
             }
-            else if(nums[start]+nums[end]>k){
-                end--;
-            }
-            else{
-                start++;
+            else if(mpp.find(rem)!=mpp.end() && mpp[rem]>0 && mpp[nums[i]]>0){
+                mpp[nums[i]]--,mpp[rem]--,ans++;
             }
         }
         return ans;
