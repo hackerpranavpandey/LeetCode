9f2fa06d768843fca5efda26f1912d9b56de078b
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-05 13:16:14 +0530

    Time: 11 ms (38.85%), Space: 8.8 MB (50.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,28 +6,28 @@ public:
         // so simple just make a window of size as s1 in s2
         // now check that it is anagram of s1
         // continue this till end of the s2
-        if(s2.size()<s1.size())   // case whenn s2 size is less then not possible
+        if(s2.size()<s1.size())   // case when s2 size is less then not possible
             return false;
-        unordered_map<char,int> mpp_1,mpp_2;
+        vector<int> freq_1(26,0),freq_2(26,0);
         for(char c:s1){
-            mpp_1[c]+=1;
+            freq_1[c-'a']+=1;
         }
         int n=s1.size();
         for(int i=0;i<n;i++){
-            mpp_2[s2[i]]++;
-        }
+            freq_2[s2[i]-'a']++;
+        } 
         int count=0;
-        for(auto it:mpp_1){
-                if(mpp_1[it.first]==mpp_2[it.first])
-                    count+=it.second;
+        for(int i=0;i<26;i++){
+                if(freq_1[i]==freq_2[i])
+                    count+=freq_1[i];
             }
         if(count==n)
             return true;
         for(int i=n;i<s2.size();i++){
-            mpp_2[s2[i]]++,mpp_2[s2[i-n]]--,count=0;
-            for(auto it:mpp_1){
-                if(mpp_1[it.first]==mpp_2[it.first])
-                    count+=it.second;
+            freq_2[s2[i]-'a']++,freq_2[s2[i-n]-'a']--,count=0;
+            for(int j=0;j<26;j++){
+                if(freq_1[j]==freq_2[j])
+                    count+=freq_1[j];
             }
             if(count==n)
                 return true;
