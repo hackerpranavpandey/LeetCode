c80672f4325458da2229cb9d3160cd51444cadac
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-06 23:43:26 +0530

    Time: 5 ms (8.76%), Space: 8.8 MB (16.13%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     bool areSentencesSimilar(string sentence1, string sentence2) {
         // these two vectors will store the words of both sentence resp
-        vector<string> v_1,v_2;
+        // so the code is really easy
+        // the idea is very simple first store words of both sentence in v_1 and v_2 respectively
+        // then find the prefix and suffix in both of them
+        // add the prefix and suffix if it is eual to any one of the sentence then return true
+        // else false keep i mind various corner cases
+        vector<string> v_1,v_2;  // store the words in v_1 and v_2
         int n=sentence1.size(),m=sentence2.size();
         int i=0;
         string s_1="";
@@ -18,6 +23,7 @@ public:
         }
         s_1="";
         i=0;
+        // now words of sentence2 
         while(i<m){
             if(sentence2[i]!=' ')
                 s_1+=sentence2[i];
@@ -31,7 +37,7 @@ public:
         // common prefix finding
         int t_1=v_1.size(),t_2=v_2.size();
         string prefix="";
-        unordered_set<int> set_1,set_2;
+        unordered_set<int> set_1,set_2;  // to ensure that same words is not counted as suffix also
         for(int i=0;i<min(t_1,t_2);i++){
             if(v_1[i]==v_2[i]){
                 if(prefix!="")
@@ -42,9 +48,10 @@ public:
             else
                 break;
         }
-        // common suffix finding 
+        // now finding the suffix for both of them
         string suffix="";
         for(int i=0;i<min(t_1,t_2);i++){
+            // make sure that it has not been included in suffix
             if(v_1[t_1-i-1]==v_2[t_2-i-1] && !set_1.count(t_1-i-1) && !set_2.count(t_2-i-1)){
                 if(suffix=="")
                     suffix=v_1[t_1-1-i];
@@ -54,6 +61,7 @@ public:
             else
                 break;
         }
+        // different cases for the prefix and suffix
         if(prefix=="" && suffix=="")
             return false;
         else if(prefix=="")
