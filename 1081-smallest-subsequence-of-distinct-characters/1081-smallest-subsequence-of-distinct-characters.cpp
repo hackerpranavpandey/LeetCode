b52b388942b4661bd06b6c17f76abf955f2345ff
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-08 02:04:04 +0530

    Time: 0 ms (100.00%), Space: 8 MB (55.90%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        int n=s.size();
-        if(n==1)
-            return s;
-        vector<int> index(26,0); //store the last index of each of char
-        for(int i=0;i<n;i++){
-            index[s[i]-'a']=i;
-        }
-        vector<int> present(26,0); // to track if present char is already in the stack
-        stack<char> t;
-        t.push(s[0]);
-        present[s[0]-'a']=1;
         // now so easy use the logic if already present no need to insert
         // if greater that t.top() directly insert directly else check if it is present later
         // and current s[i] is less than t.top() then pop till it is possible to get later
         // also check for s[i] should be less than t.top()
+        int n=s.size();
+        if(n==1)
+            return s;
+        vector<int> index(26,0);
+        for(int i=0;i<n;i++){
+            index[s[i]-'a']=i;
+        }
+        vector<int> present(26,0);
+        stack<char> t;
+        t.push(s[0]);
+        present[s[0]-'a']=1;
         for(int i=1;i<n;i++){
             if(present[s[i]-'a'])
                 continue;
