f882596c7288f20be5221d5885815d14572cebe6
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-09 15:35:14 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        // simple use the same the valid paranthesis
+        // if ')' comes before '(' then add 1 to ans and at end check extra '(' brackets left
         int ans=0;
         stack<char> s_t;
         for(char c:s){
