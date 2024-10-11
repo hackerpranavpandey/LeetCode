4151f164de2092c0f3d32702ec491baaaed0f83e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-11 23:48:24 +0530

    Time: 4 ms (98.96%), Space: 24.1 MB (79.63%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
+        // total 3 ways to solve this question - O(n^2) , T=O(n),S=O(n), O(n)
         // for(int i=0;i<nums.size();i++){
         //     if((i%2==0 && nums[i]%2==0) || (i%2==1 && nums[i]%2==1))
         //         continue;
@@ -24,15 +25,29 @@ public:
         //     }
         // }
         // return nums;
-        int n=nums.size();
-        vector<int> ans(n);
-        int even=0,odd=1;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0)
-                ans[even]=nums[i],even+=2;
-            else
-                ans[odd]=nums[i],odd+=2;
+        // O(n) time and O(n) space
+        // int n=nums.size();
+        // vector<int> ans(n);
+        // int even=0,odd=1;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2==0)
+        //         ans[even]=nums[i],even+=2;
+        //     else
+        //         ans[odd]=nums[i],odd+=2;
+        // }
+        // return ans;
+        int evenIndex=0,oddIndex=1,n=nums.size();
+        while(evenIndex<n && oddIndex<n){
+            while(evenIndex<n && nums[evenIndex]%2==0){
+                evenIndex+=2;
+            }
+            while(oddIndex<n && nums[oddIndex]%2==1){
+                oddIndex+=2;
+            }
+            if(evenIndex<n && oddIndex<n){
+                swap(nums[evenIndex],nums[oddIndex]);
+            }
         }
-        return ans;
+        return nums;
     }
 };
