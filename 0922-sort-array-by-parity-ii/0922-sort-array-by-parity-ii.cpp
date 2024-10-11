07670340ad65156d67da63fb2d3e0d4d17e04a8d
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-11 23:29:13 +0530

    Time: 15 ms (48.65%), Space: 24.2 MB (44.15%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            if((i%2==0 && nums[i]%2==0) || (i%2==1 && nums[i]%2==1))
-                continue;
-            else if(i%2==0){
-                // swap with first odd
-                for(int j=i+1;j<nums.size();j++){
-                    if(nums[j]%2==0){
-                        swap(nums[i],nums[j]);
-                        break;
-                    }
-                }
-            }
-            else{
-                // swap with first even
-                for(int j=i+1;j<nums.size();j++){
-                    if(nums[j]%2==1){
-                        swap(nums[i],nums[j]);
-                        break;
-                    }
-                }
-            }
+        // for(int i=0;i<nums.size();i++){
+        //     if((i%2==0 && nums[i]%2==0) || (i%2==1 && nums[i]%2==1))
+        //         continue;
+        //     else if(i%2==0){
+        //         // swap with first odd
+        //         for(int j=i+1;j<nums.size();j++){
+        //             if(nums[j]%2==0){
+        //                 swap(nums[i],nums[j]);
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     else{
+        //         // swap with first even
+        //         for(int j=i+1;j<nums.size();j++){
+        //             if(nums[j]%2==1){
+        //                 swap(nums[i],nums[j]);
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        // return nums;
+        int n=nums.size();
+        vector<int> ans(n);
+        int even=0,odd=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0)
+                ans[even]=nums[i],even+=2;
+            else
+                ans[odd]=nums[i],odd+=2;
         }
-        return nums;
+        return ans;
     }
 };
