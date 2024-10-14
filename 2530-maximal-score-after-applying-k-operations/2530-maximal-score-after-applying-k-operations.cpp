3606a84fdad91729cdcc3447376347ca6a593c02
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-14 12:52:22 +0530

    Time: 210 ms (17.71%), Space: 79.8 MB (21.70%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        long long ans=0;
+        priority_queue<int> storeNum;
+        int n=nums.size();
+        for(auto& num:nums){
+            storeNum.push(num);
+        }
+        while(k>0){
+            int curr=storeNum.top();
+            storeNum.pop();
+            ans+=curr;
+            curr=ceil((double)curr/(double)3);
+            storeNum.push(curr);
+            k--;
+        }
+        return ans;
+    }
+};
