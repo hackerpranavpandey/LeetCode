3ccb9a9962c7c097e1949d7657385e6af1e80ddc
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-17 20:51:12 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (70.34%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -7,15 +7,19 @@ public:
         int n=t.size();
         vector<int> maxRightIndex(n,0);
         maxRightIndex[n-1]=n-1;
+        char max=t[n-1];
+        int index=n-1;
         for(int i=n-2;i>=0;i--){
-            if(t[i]>t[maxRightIndex[i+1]])
-                maxRightIndex[i]=i;
+            if(t[i]>max){
+                max=t[i];
+                index=i;
+                maxRightIndex[i]=index;
+            }
             else
-                maxRightIndex[i]=maxRightIndex[i+1];
+                maxRightIndex[i]=index;
         }
         for(int i=0;i<n;i++){
-            // also check if same digits are not swapped
-            if(maxRightIndex[i]!=i && t[i]!=t[maxRightIndex[i]]){
+            if(maxRightIndex[i]!=i && t[maxRightIndex[i]]>t[i]){
                 swap(t[i],t[maxRightIndex[i]]);
                 return stoi(t);
             }
