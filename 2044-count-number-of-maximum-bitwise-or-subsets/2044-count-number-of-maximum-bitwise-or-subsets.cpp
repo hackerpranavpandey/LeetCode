68b70cac694d69e4e17cfe7a139cf5269cee89e6
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-18 15:11:25 +0530

    Time: 79 ms (22.22%), Space: 10.2 MB (31.38%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-        // lets find the maximum abitwise or among all subsets;
+        // lets find the maximum bitwise or among all subsets
+        // then find number of such subsets
         int n=nums.size();
         int totalSubset=pow(2,n)-1;
         int max_value=INT_MIN,ans=0;
-        // total number of subset is 2^n-1
         for(int i=0;i<=totalSubset;i++){
             int curr=0;
             for(int j=0;j<n;j++){
