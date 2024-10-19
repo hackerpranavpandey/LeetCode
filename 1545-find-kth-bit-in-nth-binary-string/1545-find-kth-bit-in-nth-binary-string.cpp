5daa46818e825064606bd922daed8415cc97c01e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-19 12:17:54 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (64.16%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -12,16 +12,26 @@ private:
     }
 public:
     char findKthBit(int n, int k) {
-        string prev="0";
-        string curr="0";
-        if(n==1)
-            return prev[0];
-        for(int i=0;i<n;i++){
-            string t=invert(prev);
-            reverse(t.begin(),t.end());
-            curr=(prev+ '1') + t;
-            prev=curr;
+        // string S1="0";
+        // if(n==1)
+        //     return S1[0];
+        // for(int i=0;i<n;i++){
+        //     string t=invert(prev);
+        //     reverse(t.begin(),t.end());
+        //     curr=(prev+ '1') + t;
+        //     prev=curr;
+        // }
+        // return curr[k-1];
+        if(n == 1)
+            return '0';
+        int len=1 << n;
+        if(k == len/2)
+            return '1';
+        else if(k < len/2)
+            return findKthBit(n-1, k);
+        else{
+            int kBit = findKthBit(n-1, len-k);
+            return (kBit == '0') ? '1' : '0';
         }
-        return curr[k-1];
     }
 };
