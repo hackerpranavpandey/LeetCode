2c3e13c4852e2201421043fa45ef65ac11ed6342
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-20 22:08:42 +0530

    Time: 5 ms (75.48%), Space: 10.8 MB (47.48%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    bool performOperation(string t,char operation){
+        if(operation=='!')
+            return t=="f";
+        else if(operation=='|'){
+            for(char c:t){
+                if(c == 't')
+                    return true;
+            }
+            return false;;
+        }
+        else{
+            for(char c:t){
+                if(c == 'f')
+                    return false;
+            }
+            return true;
+        }
+    }
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> opr;
+        stack<char> operand;
+        int n=expression.size();
+        for(int i=0;i<n;i++){
+            if(expression[i]=='&' || expression[i]=='!' || expression[i]=='|'){
+                opr.push(expression[i]);
+            }
+            if(expression[i]=='(')
+                operand.push(expression[i]);
+            else if(expression[i]==')'){
+                string t="";
+                while(operand.top()!='('){
+                    t+=operand.top();
+                    operand.pop();
+                }
+                operand.pop();
+                char operation=opr.top();
+                opr.pop();
+                bool ans=performOperation(t,operation);
+                ans == true ? operand.push('t') : operand.push('f');
+            }
+            else if(expression[i]=='t' || expression[i]=='f')
+                operand.push(expression[i]);
+        }
+        return operand.top() == 't';
+    }
+};
