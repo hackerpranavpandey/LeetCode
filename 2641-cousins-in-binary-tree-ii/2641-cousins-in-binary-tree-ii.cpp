8f6ef556d17361372470375fdca871a30fad4f5e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-23 13:22:34 +0530

    Time: 66 ms (94.65%), Space: 340.7 MB (19.60%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        unordered_map<int,long long> levelSum;
+        queue<TreeNode*> t;
+        t.push(root);
+        int level=0;
+        while(!t.empty()){
+            int n=t.size();
+            long long sum=0;
+            for(int i=0;i<n;i++){
+                auto it=t.front();
+                t.pop();
+                sum+=it->val;
+                if(it->left)
+                    t.push(it->left);
+                if(it->right)
+                    t.push(it->right);
+            }
+            levelSum[level]=sum;
+            level++;
+        }
+        level=0;
+        t.push(root);
+        while(!t.empty()){
+            int n=t.size();
+            for(int i=0;i<n;i++){
+                auto it=t.front();
+                t.pop();
+                if(level==0 || level==1)
+                    it->val=0;
+                if(it->left && it->right){
+                    int sibling = it->left->val + it->right->val;
+                    it->left->val=levelSum[level+1]- sibling;
+                    it->right->val=levelSum[level+1] - sibling;
+                    t.push(it->left),t.push(it->right);
+                }
+                else if(it->left && !it->right){
+                    it->left->val=levelSum[level+1] - it->left->val;
+                    t.push(it->left);
+                }
+                else if(!it->left && it->right){
+                    it->right->val=levelSum[level+1] - it->right->val;
+                    t.push(it->right);
+                }
+            }
+            level++;
+        }
+        return root;
+    }
+};
