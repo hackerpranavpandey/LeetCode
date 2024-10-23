4b1c634be97b53a3369609218feee785b62d0914
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-23 13:29:13 +0530

    Time: 58 ms (94.65%), Space: 340.6 MB (19.60%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
-        // damn its so simple just levelorder traversal and sum computation
+        // this problemis nothing but using bfs twice
         // first store the sum of the nodes in each of the level
         // for level 0 and 1 value will be 0 for each of the node
         // then for each node update the value of its child considering 3 conditions
