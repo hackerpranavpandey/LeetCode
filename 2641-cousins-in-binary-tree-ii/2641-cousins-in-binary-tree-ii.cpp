2d4a26f64fb3fb34036c66ad2da939e67aba4bfe
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-23 13:25:39 +0530

    Time: 56 ms (94.65%), Space: 340.7 MB (19.60%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
+        // damn its so simple just levelorder traversal and sum computation
+        // first store the sum of the nodes in each of the level
         unordered_map<int,long long> levelSum;
         queue<TreeNode*> t;
         t.push(root);
@@ -33,6 +35,7 @@ public:
         }
         level=0;
         t.push(root);
+        // now update the value of each node
         while(!t.empty()){
             int n=t.size();
             for(int i=0;i<n;i++){
@@ -40,16 +43,19 @@ public:
                 t.pop();
                 if(level==0 || level==1)
                     it->val=0;
+                // if both child exist then update both of them value
                 if(it->left && it->right){
                     int sibling = it->left->val + it->right->val;
                     it->left->val=levelSum[level+1]- sibling;
                     it->right->val=levelSum[level+1] - sibling;
                     t.push(it->left),t.push(it->right);
                 }
+                // in case only left child exist
                 else if(it->left && !it->right){
                     it->left->val=levelSum[level+1] - it->left->val;
                     t.push(it->left);
                 }
+                // in case only right child exits
                 else if(!it->left && it->right){
                     it->right->val=levelSum[level+1] - it->right->val;
                     t.push(it->right);
