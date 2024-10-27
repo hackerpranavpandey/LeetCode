5d60ef1bc4c2d5a3aa6727e7be781766a3cf094e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-27 12:18:15 +0530

    Time: 283 ms (5.35%), Space: 26.4 MB (92.75%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    // given the value of t1 and matrix it return s umber of square matrix of size t1*t1
+    int solve(int t1,vector<vector<int>>& matrix){
+        int m=matrix.size(),n=matrix[0].size();
+        int rowStart=0,rowEnd=t1-1;
+        int totalSquare=0;
+        while(rowEnd<m){
+            int colStart=0,colEnd=t1-1;
+            while(colEnd<n){
+                int count=0;
+                for(int t=rowStart;t<=rowEnd;t++){
+                    for(int z=colStart;z<=colEnd;z++){
+                       if(matrix[t][z])
+                           count++;
+                       else{
+                           count=-1;
+                           break;
+                       }
+                    }
+                    if(count==-1)
+                        break;
+                }
+                if(count==pow(t1,2))
+                    totalSquare++;
+                colStart++,colEnd++;
+            }
+            rowStart++,rowEnd++;
+        }
+        return totalSquare;
+    }
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        // one way to count all possible square from size 1 to min(m,n) but it exceeded time limit
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j])
+                    ans++;
+            }
+        }
+        int maxSquare=min(m,n);
+        for(int i=2;i<=maxSquare;i++){
+            ans+=solve(i,matrix);
+        }
+        return ans;
+        
+    }
+};
