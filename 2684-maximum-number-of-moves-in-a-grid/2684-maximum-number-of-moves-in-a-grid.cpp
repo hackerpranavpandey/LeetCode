2e2353344de3d90e30e8581727c888e591394b8d
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-10-29 10:32:14 +0530

    Time: 23 ms (52.17%), Space: 74.6 MB (18.45%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    int moveNow(vector<vector<int>>& grid,int row,int col,vector<vector<int>>& dp){
+        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size()-1)
+            return 0;
+        if(dp[row][col]!=-1)
+            return dp[row][col];
+        int moveUp=0;
+        int moveDown=0;
+        int moveRight=0;
+        if(row-1>=0 && grid[row-1][col+1]>grid[row][col])
+            moveUp=1+moveNow(grid,row-1,col+1,dp);
+        if(row+1<grid.size() && grid[row+1][col+1]>grid[row][col])
+           moveDown=1+moveNow(grid,row+1,col+1,dp);
+        if(grid[row][col+1]>grid[row][col])
+           moveRight=1+moveNow(grid,row,col+1,dp);
+        dp[row][col]=max({moveUp,moveDown,moveRight});
+        return dp[row][col];
+    }
+public:
+    int maxMoves(vector<vector<int>>& grid) {
+        // observe that it always has to move to the next column
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,moveNow(grid,i,0,dp));
+        }
+        return ans;
+    }
+};
