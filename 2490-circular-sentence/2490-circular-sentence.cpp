53002627fc80a5fa7ab3a1909aea5e87eee3b580
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-02 10:20:54 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (96.30%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -7,6 +7,7 @@ public:
         int i=0;
         char last=' ';
         while(i<n){
+            // damn first check if it is first char of any sentence then it should be equal to last then update
             if(i>0 && i<=n-1 && sentence[i-1]==' ' && last!=sentence[i])
                 return false;
             if(i+1<=n-1 && sentence[i+1]==' ')
