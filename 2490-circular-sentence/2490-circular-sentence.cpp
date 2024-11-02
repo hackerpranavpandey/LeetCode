7ab064a57239d44080d8e2973ca26a92220503a2
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-03 00:10:38 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (59.78%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        // damn first check if it is first char of any sentence then it should be equal to last then update
         int n=sentence.size();
         if(sentence[0]!=sentence[n-1] || sentence[0]==' ' || sentence[n-1]==' ')
             return false;
-        int i=0;
-        char last=' ';
-        while(i<n){
-            if(i>0 && i<=n-1 && sentence[i-1]==' ' && last!=sentence[i])
+        for(int i=1;i<n-1;i++){
+            if(sentence[i]==' ' && sentence[i-1]!=sentence[i+1])
                 return false;
-            if(i+1<=n-1 && sentence[i+1]==' ')
-                last=sentence[i];
-            i++;
         }
         return true;
     }
