b311b10cc24d0bae5f1e02b8bfc5621fb14aa1b3
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-04 12:18:51 +0530

    Time: 57 ms (23.93%), Space: 30 MB (9.87%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
     string compressedString(string word) {
+        // below code is correct but it has very bad readibility
+        // int n=word.size();
+        // string ans="";
+        // char c=word[0];
+        // int index=1;
+        // int count=1;
+        // while(index<n){
+        //     if(word[index]==c)
+        //         count++;
+        //     else{
+        //         ans+=(to_string(count)+c);
+        //         c=word[index];
+        //         count=1;
+        //     }
+        //     index++;
+        //     if(count==9 && index!=n){
+        //         ans+=(to_string(count)+c);
+        //         count=0;
+        //         // if exactly 9 then it will insert extra 0 and corresponding char
+        //         if(word[index]!=c){
+        //             c=word[index];
+        //             count=1;
+        //             index++;
+        //         }
+        //     }
+        // }
+        // ans+=(to_string(count)+c);
+        // return ans;
         int n=word.size();
-        string ans="";
-        char c=word[0];
-        int index=1;
-        int count=1;
-        while(index<n){
-            if(word[index]==c)
+        string comp="";
+        int pos=0;
+        while(pos<n){
+            int count=0;
+            char current=word[pos];
+            while(pos<n && count<9 && word[pos]==current){
+                pos++;
                 count++;
-            else{
-                ans+=(to_string(count)+c);
-                c=word[index];
-                count=1;
             }
-            index++;
-            if(count==9 && index!=n){
-                ans+=(to_string(count)+c);
-                count=0;
-                // if exactly 9 then it will insert extra 0 and corresponding char
-                if(word[index]!=c){
-                    c=word[index];
-                    count=1;
-                    index++;
-                }
-            }
+            comp+=(to_string(count)+current);
         }
-        ans+=(to_string(count)+c);
-        return ans;
+        return comp;
     }
 };
