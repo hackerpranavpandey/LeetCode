c3fcb7862270b3a4c3695b23f4328e2c0a7ce8bb
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-05 10:13:32 +0530

    Time: 4 ms (30.85%), Space: 12.3 MB (16.88%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minChanges(string s) {
+        // just simple logic take substring of size 2 if both are different then need 1 changes
         int ans=0;
         int index=0;
         while(index<s.size()){
