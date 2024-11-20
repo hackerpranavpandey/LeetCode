40d48799fb6c11d615fda3a6782ee3453fa3085e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-20 10:40:17 +0530

    Time: 14 ms (60.49%), Space: 12 MB (80.12%) - LeetHub

---
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        vector<int> t(3,0);
+        for(int i=0;i<s.size();i++){
+            t[s[i]-'a']++;
+        }
+        for(int i=0;i<3;i++){
+            if(t[i]<k)
+                return -1;
+        }
+        vector<int> window(3, 0);
+        int left = 0, maxWindow = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            window[s[right] - 'a']++;
+
+            // Shrink window if we take too many characters
+            while (left <= right &&
+                   (t[0] - window[0] < k || t[1] - window[1] < k ||
+                    t[2] - window[2] < k)) {
+                window[s[left] - 'a']--;
+                left++;
+            }
+
+            maxWindow = max(maxWindow, right - left + 1);
+        }
+
+        return s.size() - maxWindow;
+    }
+};
