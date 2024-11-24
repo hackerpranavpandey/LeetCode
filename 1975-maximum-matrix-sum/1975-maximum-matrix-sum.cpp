beb9889776c136616e37dd2da2c835d511b400c0
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-24 10:43:40 +0530

    Time: 1 ms (53.23%), Space: 38.4 MB (30.74%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
+        // so it is simple enough just to maximize the sum of the value
+        // so for that check if there are even number of negative numbers
+        // if even then there is way to make all of them positive
+        // for add case check minimum abs(matrix[i][j]) and except that all will be part of the ans
         long long ans=0;
         int min_t=INT_MAX;
         int count=0;
