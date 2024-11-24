3e49f487a69fa060093d0246d56cb592982cf8f3
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-24 10:32:59 +0530

    Time: 4 ms (31.34%), Space: 38.5 MB (30.74%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long ans=0;
+        int min_t=INT_MAX;
+        int count=0;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                ans+=abs(matrix[i][j]);
+                min_t=min(min_t,abs(matrix[i][j]));
+                count+=(matrix[i][j]<0);
+            }
+        }
+        if(count%2==0)
+            return ans;
+        return (ans-2*min_t);
+    }
+};
