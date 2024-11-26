6f0c0f002c13f56aa5409bca0f37b2a554faf5d5
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-11-26 13:47:33 +0530

    Time: 57 ms (21.03%), Space: 105.5 MB (15.48%) - LeetHub

---
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        unordered_map<int,int> indegree;
+        for(vector<int> edge:edges){
+            int u=edge[0],v=edge[1];
+            indegree[v]++;
+        }
+        int ans=-1;
+        for(int i=0;i<n;i++){
+            if(!indegree.count(i)){
+                if(ans!=-1)
+                    return -1;
+                ans=i;
+            }
+        }
+        return ans;
+    }
+};
