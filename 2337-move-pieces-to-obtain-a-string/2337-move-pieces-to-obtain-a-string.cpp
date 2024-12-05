083acfe250d7509956c0cd66cc79a33ea929e927
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-05 12:11:20 +0530

    Time: 30 ms (29.89%), Space: 40.2 MB (6.30%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -7,43 +7,19 @@ public:
         // just opposite for the 'R'
         int n=start.size();
         string s1="",s2="";
+        vector<int> start_index, target_index;
         for(int i=0;i<n;i++){
             if(start[i]!='_')
-                s1+=start[i];
+                s1+=start[i], start_index.push_back(i);
             if(target[i]!='_')
-                s2+=target[i];
+                s2+=target[i], target_index.push_back(i);
         }
         if(s1!=s2)
             return false;
-         queue<pair<char, int>> startQueue, targetQueue;
-
-        // Record non-underscore characters and their indices
-        for (int i = 0; i < start.size(); i++) {
-            if (start[i] != '_') {
-                startQueue.push({start[i], i});
-            }
-            if (target[i] != '_') {
-                targetQueue.push({target[i], i});
-            }
-        }
-
-        // If number of pieces don't match, return false
-        if (startQueue.size() != targetQueue.size()) return false;
-
-        // Compare each piece's type and position
-        while (!startQueue.empty()) {
-            auto [startChar, startIndex] = startQueue.front();
-            startQueue.pop();
-            auto [targetChar, targetIndex] = targetQueue.front();
-            targetQueue.pop();
-
-            // Check character match and movement rules
-            if (startChar != targetChar ||
-                (startChar == 'L' && startIndex < targetIndex) ||
-                (startChar == 'R' && startIndex > targetIndex))
+        for(int i=0;i<s1.size();i++){
+            if((s1[i]=='L' && start_index[i]<target_index[i]) || (s1[i]=='R' && start_index[i]>target_index[i])) 
                 return false;
         }
-
         return true;
     }
 };
