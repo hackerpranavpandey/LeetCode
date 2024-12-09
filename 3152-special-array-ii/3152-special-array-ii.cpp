8be7c5b59dc10199cd1f40283aff510592a2812a
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-09 15:11:05 +0530

    Time: 47 ms (19.92%), Space: 142.6 MB (5.15%) - LeetHub

---
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        // simple logic of using the array number they belongs to
+        // just got this idea after reading the hints
+        unordered_map<int,int> t;
+        // values can be same so use index as key
+        t[0]=0;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if((nums[i-1]%2)!=(nums[i]%2))
+                t[i]=t[i-1];
+            else
+                t[i]=t[i-1]+1;
+        }
+        int t2=queries.size();
+        vector<bool> ans(t2,false);
+        for(int i=0;i<t2;i++){
+            int startIndex=queries[i][0],endIndex=queries[i][1];
+            if(t[startIndex]==t[endIndex])
+                ans[i]=true;
+        }
+        return ans;
+    }
+};
