84c324a76f122aeaf73e50439a9be886353e657e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-11 10:06:18 +0530

    Time: 40 ms (86.10%), Space: 104.9 MB (84.24%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         int ans=0;
-        int t1=0, t2=0;
         int j=0;
         for(int i=0;i<n;i++){
             while(j<n && nums[j]-nums[i] <= 2*k){
