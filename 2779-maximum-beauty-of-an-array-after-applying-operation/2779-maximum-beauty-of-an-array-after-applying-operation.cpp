cbad0e4d72a768125584db731e689efa8225997e
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-11 10:06:06 +0530

    Time: 51 ms (60.73%), Space: 104.9 MB (94.06%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        int t1=0, t2=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            while(j<n && nums[j]-nums[i] <= 2*k){
+                j++;
+            }
+            ans=max(ans,j-i);
+        }
+        return ans;
+    }
+};
