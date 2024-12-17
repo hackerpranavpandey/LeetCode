bcc4c375028bef5164e0b4c94bedd38128acc941
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-17 11:51:18 +0530

    Time: 47 ms (49.62%), Space: 28.3 MB (25.74%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        unordered_map<char,int> freq_count;
+        for(char c:s){
+            freq_count[c]++;
+        }
+        priority_queue<pair<char,int>> lexico_order;
+        for(auto &it:freq_count){
+            lexico_order.push({it.first,it.second});
+        }
+        string ans="";
+        while(!lexico_order.empty()){
+            pair<char,int> curr=lexico_order.top();
+            lexico_order.pop();
+            if(curr.second<=repeatLimit){
+                for(int i=0;i<curr.second;i++){
+                    ans+=curr.first;
+                }
+            }
+            else{
+                for(int i=0;i<repeatLimit;i++){
+                    ans+=curr.first;
+                }
+                curr.second-=repeatLimit;
+                if(lexico_order.empty())
+                    return ans;
+                pair<char,int> curr_2=lexico_order.top();
+                lexico_order.pop();
+                ans+=curr_2.first;
+                curr_2.second-=1;
+                if(curr_2.second>0)
+                    lexico_order.push(curr_2);
+                lexico_order.push(curr);
+            }
+        }
+        return ans;
+    }
+};
