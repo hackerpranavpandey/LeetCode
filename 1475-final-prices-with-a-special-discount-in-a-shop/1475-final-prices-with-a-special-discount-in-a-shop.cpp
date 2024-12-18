4c0db73b5bb1a066d0f1d0804ee0bfe7db8bbbc2
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-18 11:09:30 +0530

    Time: 1 ms (23.39%), Space: 14 MB (24.39%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        // brute force solution also works but using monotonic stack is better in O(n) time
+        vector<int> ans(prices.begin(),prices.end());
         int n=prices.size();
+        stack<int> st;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(prices[j]<=prices[i]){
-                    prices[i]-=prices[j];
-                    break;
-                }
+            while(!st.empty() && prices[st.top()]>=prices[i]){
+                ans[st.top()]-=prices[i];
+                st.pop();
             }
+            st.push(i);
         }
-        return prices;
+        return ans;
     }
 };
