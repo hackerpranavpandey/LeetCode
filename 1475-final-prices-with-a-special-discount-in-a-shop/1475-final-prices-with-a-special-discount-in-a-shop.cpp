6cd339a7a360ea369b72c8cab923378944005c51
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-18 10:56:21 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (24.39%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> ans;
         int n=prices.size();
         for(int i=0;i<n;i++){
-            ans.push_back(prices[i]);
             for(int j=i+1;j<n;j++){
                 if(prices[j]<=prices[i]){
-                    ans[i]-=prices[j];
+                    prices[i]-=prices[j];
                     break;
                 }
             }
         }
-        return ans;
+        return prices;
     }
 };
