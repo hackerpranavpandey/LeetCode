9ca0688f4dff87863ee8c0a2eb0ec609f3b5f073
Pranav Kumar Pandey <146061024+hackerpranavpandey@users.noreply.github.com>
2024-12-21 13:29:34 +0530

    Time: 7 ms (53.61%), Space: 15.3 MB (22.21%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        // this is O(2N) time as well as space we cannot assign random f the node has not been created
+        // one way to avoid this is insert newnode in between then traverse again
+        unordered_map<Node*,Node*> mpp;
+        Node* temp=head;
+        while(temp){
+            Node* newNode=new Node(temp->val);
+            mpp[temp]=newNode;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp){
+            Node* curr=mpp[temp];
+            curr->next=mpp[temp->next];
+            curr->random=mpp[temp->random];
+            temp=temp->next;
+        }
+        return mpp[head];
+    }
+};
